a3eae824c45354709d5e9404d02db5ae7012a9e7
Gustavo Acuna <gustavo.acuna-contractro@fabfitfun.com>
2024-03-28 10:28:19 -0300

    Initial commit

---
+++ src/main/kotlin/com/k2pay/safetypayintegrationapi/SafetypayIntegrationApiApplication.kt
@@ -0,0 +1,11 @@
+package com.k2pay.safetypayintegrationapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SafetypayIntegrationApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<SafetypayIntegrationApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/k2pay/safetypayintegrationapi/SafetypayIntegrationApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.k2pay.safetypayintegrationapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SafetypayIntegrationApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
