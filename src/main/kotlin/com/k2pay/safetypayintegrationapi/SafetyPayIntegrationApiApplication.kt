2c3f34d364e6b2e0af82807dca2b6e1ca89de84e
Gustavo Acuna <gustavo.acuna-contractro@fabfitfun.com>
2024-03-28 10:32:59 -0300

    fixed typo

--- src/main/kotlin/com/k2pay/safetypayintegrationapi/SafetyPayIntegrationApiApplication.kt
+++ src/main/kotlin/com/k2pay/safetypayintegrationapi/SafetyPayIntegrationApiApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SafetypayIntegrationApiApplication
+class SafetyPayIntegrationApiApplication
 
 fun main(args: Array<String>) {
-	runApplication<SafetypayIntegrationApiApplication>(*args)
+	runApplication<SafetyPayIntegrationApiApplication>(*args)
 }
